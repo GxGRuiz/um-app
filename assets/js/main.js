2fc3fdcae672eec56d0a5d61d5df78b72a5b27e8
g.ruiz <g.ruiz@ictmaatwerk.com>
2022-04-05 15:48:07 +0200

    splitting of responsibilities

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,77 @@ let base, baseUrl, posterSizes, backdropSizes, genres, pages;
 searchBar.addEventListener('keyup', function () {
   movieSearch();
 })
+
+const urlCall = ApiCall("https://api.themoviedb.org/3/configuration?api_key=1f54bd990f1cdfb230adb312546d765d")
+  .then(function (result) {
+    console.log(result);
+    baseUrl = result.images.secure_base_url;
+    posterSizes = result.images.poster_sizes;
+    backdropSizes = result.images.backdrop_sizes;
+    return ApiCall("https://api.themoviedb.org/3/genre/movie/list?api_key=1f54bd990f1cdfb230adb312546d765d&language=en-US");
+  })
+  .then(function (result) {
+    genres = result.genres;
+    return ApiCall("https://api.themoviedb.org/3/discover/movie?api_key=1f54bd990f1cdfb230adb312546d765d&language=en-US&sort_by=primary_release_date.asc&include_adult=false&include_video=false&page=1&primary_release_date.gte=" + today);
+
+  })
+  .then(function (result) {
+
+    result.results.forEach(element => {
+
+      let movGenres = "";
+      let imgSrc = null;
+      (element.poster_path == null) ? imgSrc = baseUrl + backdropSizes[0] + element.backdrop_path: imgSrc = baseUrl + posterSizes[0] + element.poster_path;
+      element.genre_ids.forEach(genre_id => movGenres += genres.find((genre) => genre.id === genre_id)['name'] + ", ");
+      movGenres = movGenres.slice(0, -2);
+
+      if (movGenres.length == 0) {
+        movGenres = "<b>GENRE UNAVAILABLE";
+      }
+
+      let imgElmt;
+
+      (/null/.test(imgSrc)) ? imgElmt = "<b>IMAGE UNAVAILABLE</b>": imgElmt = `<img class='movie-poster' src=` + imgSrc + `>`;
+
+
+      tableContent.innerHTML += `<tr data-bs-toggle='modal' data-bs-target='#movieModal' class='list-item'>
+                            <td>` + element.title + `<input class='movie-title' type='hidden' value='` + element.title + `'></td>
+                            <td>` + imgElmt + `
+                               <input class='movie-overview' type='hidden' value='` + element.overview + `'>
+                            </td>
+                            <td>` + movGenres + `<input class='movie-genre' type='hidden' value='` + movGenres + `'></td>
+                            <td>` + element.release_date + `<input class='movie-release-date' type='hidden' value='` + element.release_date + `'></td>
+                        </tr>`;
+    });
+
+    [...listItems].forEach(listItem => {
+
+      listItem.addEventListener('click', function () {
+
+        let movieTitle = listItem.getElementsByClassName('movie-title')[0].value;
+        let moviePoster = listItem.getElementsByClassName('movie-poster')[0];
+        let movieOverview = listItem.getElementsByClassName('movie-overview')[0].value;
+        let movieGenres = listItem.getElementsByClassName('movie-genre')[0].value;
+        let movieReleaseDate = listItem.getElementsByClassName('movie-release-date')[0].value;
+
+        movieModalContent.innerHTML = "";
+        movieModalLabel.innerHTML = movieTitle;
+        movieModalContent.innerHTML += `<div id="movie-details">
+                                                 <div>` + movieOverview + `</div>
+                                                 <div><h6>Genre</h6>` + movieGenres + `</div>
+                                                 <div><h6>Release Date</h6>` + movieReleaseDate + `</div>
+                                            </div> `;
+
+        if (moviePoster != null) {
+          document.getElementById('movie-details').prepend(moviePoster.cloneNode(true));
+        } else {
+          let b = document.createElement("b")
+          b.innerHTML = 'IMAGE UNAVAILABLE';
+          document.getElementById('movie-details').prepend(b);
+        }
+
+      })
+
+    });
+
+  })
